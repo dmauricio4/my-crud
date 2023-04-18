b4ec935865fb094760c17d29878fe10aae3ded28
dmauricio4 <diegomarshall4@gmail.com>
2023-04-18 13:13:54 -0500

    Book Class

---
+++ src/main/java/ups/edu/ec/mycrud/model/Book.java
@@ -0,0 +1,13 @@
+package ups.edu.ec.mycrud.model;
+
+public class Book {
+
+    public final long id;
+
+    public final String name;
+
+    public Book(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
